09079688909c6bac8870dbb6769e3583f336af42
Vitor Pires <88112014+vikpires@users.noreply.github.com>
2024-06-06 14:59:21 +0000

    add first card

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 
     <>
 
-     <div>
+     <div className='card'>
       <h2>Rick Sanchez</h2>
       <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"></img>
     </div>
