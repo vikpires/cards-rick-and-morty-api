212f71110eeee3d7a32f5e29ec61adce389ec59a
Vitor Pires <88112014+vikpires@users.noreply.github.com>
2024-06-07 14:34:27 +0000

    add card props

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,13 +1,14 @@
-export default function Card() {
-    return (
+export default function Card(props) {
+    const item = props.item //espaço para variáveis e funções
+    return ( //Somente jsx - exibir os componentes
         <div className='card'>
-            <h2>Rick Sanchez</h2>
+            <h2>{item.name}</h2>
             <div className="tags">
                 <div>Status: Vivo</div>
                 <div>Espécie: Humana</div>
                 <div>Origem: Terra C-137</div>
             </div>
-            <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"></img>
+            <img src={item.image}></img>
         </div>
     )
 }
