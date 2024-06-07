a05bbecbb60e01928029e0970e5f12a01eb2c272
Vitor Pires <88112014+vikpires@users.noreply.github.com>
2024-06-07 14:50:37 +0000

    adding map

--- src/App.jsx
+++ src/App.jsx
@@ -15,13 +15,15 @@ function App() {
     name: 'Summer Smith',
     image: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg'
   }
+  const itens = [item1, item2, item3]
 
   return (
     <>
     <div className="cards"> 
-      <Card item={item1}/>
-      <Card item={item2} />
-      <Card item={item3} />
+      {itens.map(function(elemento){
+        return <Card item={elemento} />
+      })}
+
     </div>
 
 
